f5365968cad733da130d58139d242e1a305e8bf6
Kolem2602 <emakolaric2602@gmail.com>
2019-05-14 20:47:02 +0200

    Add project files.

---
+++ priprema_za_klkv/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace priprema_za_klkv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
